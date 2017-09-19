c828ae85056708ae78d051a3282bdc799645bb09
Ken Kotch <hello@ken.uno>
2017-09-19 15:17:52 -0600

    dump, queries, roles done. working on manipulation

--- database_dump.sql
+++ database_dump.sql
@@ -1,19 +1,3 @@
-
---
--- Database creation
---
-
-CREATE DATABASE galvanize_contacts WITH TEMPLATE = template0 OWNER = iguana;
-REVOKE CONNECT,TEMPORARY ON DATABASE galvanize_contacts FROM PUBLIC;
-CREATE DATABASE iguana WITH TEMPLATE = template0 OWNER = iguana;
-REVOKE CONNECT,TEMPORARY ON DATABASE template1 FROM PUBLIC;
-GRANT CONNECT ON DATABASE template1 TO PUBLIC;
-
-
-\connect galvanize_contacts
-
-SET default_transaction_read_only = off;
-
 --
 -- PostgreSQL database dump
 --
@@ -31,14 +15,14 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
@@ -58,7 +42,7 @@ CREATE TABLE contacts (
     id integer NOT NULL,
     type character varying,
     value character varying,
-    user_id bigint
+    user_id integer
 );
 
 
@@ -140,12 +124,12 @@ ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regcl
 --
 
 COPY contacts (id, type, value, user_id) FROM stdin;
-2	email	cj.reynolds@galvanize.com	2
-3	email	danny.fritz@galvanize.com	3
-4	email	roberto.ortega@galvanize.com	4
-5	email	kyle.coberly@galvanize.com	5
-6	mobile	911	5
-7	home	0	3
+13	email	kyle.coberly@galvanize.com	1
+14	email	cj.reynolds@galvanize.com	2
+15	email	danny.fritz@galvanize.com	3
+16	email	roberto.ortega@galvanize.com	4
+17	mobile	911	1
+18	home	0	3
 \.
 
 
@@ -153,7 +137,7 @@ COPY contacts (id, type, value, user_id) FROM stdin;
 -- Name: contacts_id_seq; Type: SEQUENCE SET; Schema: public; Owner: iguana
 --
 
-SELECT pg_catalog.setval('contacts_id_seq', 7, true);
+SELECT pg_catalog.setval('contacts_id_seq', 18, true);
 
 
 --
@@ -161,10 +145,10 @@ SELECT pg_catalog.setval('contacts_id_seq', 7, true);
 --
 
 COPY users (id, first_name, last_name, date_of_birth, biography) FROM stdin;
+1	Kyle	Coberly	1961-02-18	Web app developer, business dork, amateur economist.
 2	CJ	Reynolds	2031-04-28	Does the man have the moustache, or does the moustache have the man?
 3	Danny	Fritz	1805-09-15	Definitely a human instructor, not a robot.
 4	Roberto	Ortega	2015-10-17	Llamas age faster than people.
-5	Kyle	Coberly	1961-02-18	Web app developer, business dork, amateur economist.
 \.
 
 
@@ -172,7 +156,7 @@ COPY users (id, first_name, last_name, date_of_birth, biography) FROM stdin;
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: iguana
 --
 
-SELECT pg_catalog.setval('users_id_seq', 5, true);
+SELECT pg_catalog.setval('users_id_seq', 4, true);
 
 
 --
@@ -200,331 +184,6 @@ ALTER TABLE ONLY contacts
 
 
 --
--- Name: contacts; Type: ACL; Schema: public; Owner: iguana
---
-
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE contacts TO galvanize_admin;
-
-
---
--- Name: users; Type: ACL; Schema: public; Owner: iguana
---
-
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE users TO galvanize_admin;
-
-
---
--- Name: DEFAULT PRIVILEGES FOR TABLES; Type: DEFAULT ACL; Schema: public; Owner: iguana
---
-
-ALTER DEFAULT PRIVILEGES FOR ROLE iguana IN SCHEMA public REVOKE ALL ON TABLES  FROM iguana;
-ALTER DEFAULT PRIVILEGES FOR ROLE iguana IN SCHEMA public GRANT SELECT,INSERT,DELETE,UPDATE ON TABLES  TO galvanize_admin;
-
-
---
 -- PostgreSQL database dump complete
 --
 
-\connect iguana
-
-SET default_transaction_read_only = off;
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 9.6.5
--- Dumped by pg_dump version 9.6.5
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: iris; Type: TABLE; Schema: public; Owner: iguana
---
-
-CREATE TABLE iris (
-    id integer NOT NULL,
-    sepal_length numeric NOT NULL,
-    sepal_width numeric NOT NULL,
-    petal_length numeric NOT NULL,
-    petal_width numeric NOT NULL,
-    species character varying NOT NULL
-);
-
-
-ALTER TABLE iris OWNER TO iguana;
-
---
--- Name: iris_id_seq; Type: SEQUENCE; Schema: public; Owner: iguana
---
-
-CREATE SEQUENCE iris_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE iris_id_seq OWNER TO iguana;
-
---
--- Name: iris_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: iguana
---
-
-ALTER SEQUENCE iris_id_seq OWNED BY iris.id;
-
-
---
--- Name: iris id; Type: DEFAULT; Schema: public; Owner: iguana
---
-
-ALTER TABLE ONLY iris ALTER COLUMN id SET DEFAULT nextval('iris_id_seq'::regclass);
-
-
---
--- Data for Name: iris; Type: TABLE DATA; Schema: public; Owner: iguana
---
-
-COPY iris (id, sepal_length, sepal_width, petal_length, petal_width, species) FROM stdin;
-51	7.0	3.2	4.7	1.4	versicolor
-52	6.4	3.2	4.5	1.5	versicolor
-53	6.9	3.1	4.9	1.5	versicolor
-54	5.5	2.3	4.0	1.3	versicolor
-55	6.5	2.8	4.6	1.5	versicolor
-56	5.7	2.8	4.5	1.3	versicolor
-57	6.3	3.3	4.7	1.6	versicolor
-58	4.9	2.4	3.3	1.0	versicolor
-59	6.6	2.9	4.6	1.3	versicolor
-60	5.2	2.7	3.9	1.4	versicolor
-61	5.0	2.0	3.5	1.0	versicolor
-62	5.9	3.0	4.2	1.5	versicolor
-63	6.0	2.2	4.0	1.0	versicolor
-64	6.1	2.9	4.7	1.4	versicolor
-65	5.6	2.9	3.6	1.3	versicolor
-66	6.7	3.1	4.4	1.4	versicolor
-67	5.6	3.0	4.5	1.5	versicolor
-68	5.8	2.7	4.1	1.0	versicolor
-69	6.2	2.2	4.5	1.5	versicolor
-70	5.6	2.5	3.9	1.1	versicolor
-71	5.9	3.2	4.8	1.8	versicolor
-72	6.1	2.8	4.0	1.3	versicolor
-73	6.3	2.5	4.9	1.5	versicolor
-74	6.1	2.8	4.7	1.2	versicolor
-75	6.4	2.9	4.3	1.3	versicolor
-76	6.6	3.0	4.4	1.4	versicolor
-77	6.8	2.8	4.8	1.4	versicolor
-78	6.7	3.0	5.0	1.7	versicolor
-79	6.0	2.9	4.5	1.5	versicolor
-80	5.7	2.6	3.5	1.0	versicolor
-81	5.5	2.4	3.8	1.1	versicolor
-82	5.5	2.4	3.7	1.0	versicolor
-83	5.8	2.7	3.9	1.2	versicolor
-84	6.0	2.7	5.1	1.6	versicolor
-85	5.4	3.0	4.5	1.5	versicolor
-86	6.0	3.4	4.5	1.6	versicolor
-87	6.7	3.1	4.7	1.5	versicolor
-88	6.3	2.3	4.4	1.3	versicolor
-89	5.6	3.0	4.1	1.3	versicolor
-90	5.5	2.5	4.0	1.3	versicolor
-91	5.5	2.6	4.4	1.2	versicolor
-92	6.1	3.0	4.6	1.4	versicolor
-93	5.8	2.6	4.0	1.2	versicolor
-94	5.0	2.3	3.3	1.0	versicolor
-95	5.6	2.7	4.2	1.3	versicolor
-96	5.7	3.0	4.2	1.2	versicolor
-97	5.7	2.9	4.2	1.3	versicolor
-98	6.2	2.9	4.3	1.3	versicolor
-99	5.1	2.5	3.0	1.1	versicolor
-100	5.7	2.8	4.1	1.3	versicolor
-101	6.3	3.3	6.0	2.5	virginica
-102	5.8	2.7	5.1	1.9	virginica
-103	7.1	3.0	5.9	2.1	virginica
-104	6.3	2.9	5.6	1.8	virginica
-105	6.5	3.0	5.8	2.2	virginica
-106	7.6	3.0	6.6	2.1	virginica
-107	4.9	2.5	4.5	1.7	virginica
-108	7.3	2.9	6.3	1.8	virginica
-109	6.7	2.5	5.8	1.8	virginica
-110	7.2	3.6	6.1	2.5	virginica
-111	6.5	3.2	5.1	2.0	virginica
-112	6.4	2.7	5.3	1.9	virginica
-113	6.8	3.0	5.5	2.1	virginica
-114	5.7	2.5	5.0	2.0	virginica
-115	5.8	2.8	5.1	2.4	virginica
-116	6.4	3.2	5.3	2.3	virginica
-117	6.5	3.0	5.5	1.8	virginica
-118	7.7	3.8	6.7	2.2	virginica
-119	7.7	2.6	6.9	2.3	virginica
-120	6.0	2.2	5.0	1.5	virginica
-121	6.9	3.2	5.7	2.3	virginica
-122	5.6	2.8	4.9	2.0	virginica
-123	7.7	2.8	6.7	2.0	virginica
-124	6.3	2.7	4.9	1.8	virginica
-125	6.7	3.3	5.7	2.1	virginica
-126	7.2	3.2	6.0	1.8	virginica
-127	6.2	2.8	4.8	1.8	virginica
-128	6.1	3.0	4.9	1.8	virginica
-129	6.4	2.8	5.6	2.1	virginica
-130	7.2	3.0	5.8	1.6	virginica
-131	7.4	2.8	6.1	1.9	virginica
-132	7.9	3.8	6.4	2.0	virginica
-133	6.4	2.8	5.6	2.2	virginica
-134	6.3	2.8	5.1	1.5	virginica
-135	6.1	2.6	5.6	1.4	virginica
-136	7.7	3.0	6.1	2.3	virginica
-137	6.3	3.4	5.6	2.4	virginica
-138	6.4	3.1	5.5	1.8	virginica
-139	6.0	3.0	4.8	1.8	virginica
-140	6.9	3.1	5.4	2.1	virginica
-141	6.7	3.1	5.6	2.4	virginica
-142	6.9	3.1	5.1	2.3	virginica
-143	5.8	2.7	5.1	1.9	virginica
-144	6.8	3.2	5.9	2.3	virginica
-145	6.7	3.3	5.7	2.5	virginica
-146	6.7	3.0	5.2	2.3	virginica
-147	6.3	2.5	5.0	1.9	virginica
-148	6.5	3.0	5.2	2.0	virginica
-149	6.2	3.4	5.4	2.3	virginica
-\.
-
-
---
--- Name: iris_id_seq; Type: SEQUENCE SET; Schema: public; Owner: iguana
---
-
-SELECT pg_catalog.setval('iris_id_seq', 150, true);
-
-
---
--- Name: iris iris_pkey; Type: CONSTRAINT; Schema: public; Owner: iguana
---
-
-ALTER TABLE ONLY iris
-    ADD CONSTRAINT iris_pkey PRIMARY KEY (id);
-
-
---
--- PostgreSQL database dump complete
---
-
-\connect postgres
-
-SET default_transaction_read_only = off;
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 9.6.5
--- Dumped by pg_dump version 9.6.5
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: postgres; Type: COMMENT; Schema: -; Owner: iguana
---
-
-COMMENT ON DATABASE postgres IS 'default administrative connection database';
-
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
---
--- PostgreSQL database dump complete
---
-
-\connect template1
-
-SET default_transaction_read_only = off;
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 9.6.5
--- Dumped by pg_dump version 9.6.5
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: template1; Type: COMMENT; Schema: -; Owner: iguana
---
-
-COMMENT ON DATABASE template1 IS 'default template for new databases';
-
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
---
--- PostgreSQL database dump complete
---
-
---
--- PostgreSQL database cluster dump complete
---
---
+++ database_queries.sql
@@ -0,0 +1,9 @@
+SELECT first_name || ' ' || last_name FROM users WHERE date_of_birth >= '2000-01-01' ORDER BY first_name ASC;
+
+SELECT biography FROM users WHERE biography LIKE '% man %';
+
+SELECT u.first_name, u.last_name, c.type, c.value FROM users u JOIN contacts c on u.id = c.user_id WHERE u.id=3;
+
+SELECT first_name FROM users WHERE id IN (1,3);
+
+SELECT u.first_name || ' ' || u.last_name, COUNT(type) FROM users u JOIN contacts c on u.id = c.user_id GROUP BY first_name, last_name;
